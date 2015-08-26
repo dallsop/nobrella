a8f242086c79e0292fb5b9f9828900d15c39aedd
dallsop <david.allsop@chicagobooth.edu>
2015-08-26 09:09:31 -0500

    0.7

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  root 'events#index'
+  # MAIN ROUTE
+  root 'nobrellas#index'
+  #-----------------------------
 
   # Routes for the Event resource:
   # CREATE
